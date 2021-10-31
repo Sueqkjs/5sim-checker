cfe09cbb7771d17e41b3906e4df9a4fda30f25b2
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-10-31 18:55:44 +0900

    sexy

---
+++ index.js
@@ -0,0 +1,30 @@
+const $ = document.querySelector.bind(document);
+const blackCount = [ 0, 1000, 2000 ];
+
+$("#check").onclick = async() => {
+  const product = $("#product").value;
+  const r = await (await fetch("https://5sim.net/v1/guest/prices?product=" + product)).text();
+  if (r?.match("product is incorrect")) return $("#result").value = "プロダクトミスってるぞあほ";
+  const res = JSON.parse(r)[product];
+  let result;
+  for (let country in res) {
+    for (let channelId in country) {
+      const channel = res[country][channelId];
+      if (blackCount.includes(channel.count)) return;
+      if (!result) result = {
+        country,
+        channelId,
+        ...channel
+      };
+      if (result.cost > channel.cost) result = {
+        country,
+        channelId,
+        ...channel
+      };
+    }
+  }
+  $("#result").value = `
+  ${product}で一番安いのは、${result.country}のチャネル ${result.channelId}です。
+  値段は${result.cost}で在庫数は${result.count}個です。
+  `;
+}
