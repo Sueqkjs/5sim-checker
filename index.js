72b2b8f8eda495ab632f666b048b0df3ab23d9e3
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-10-31 21:42:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,25 +8,36 @@ $("#check").onclick = async() => {
   ).text();
   if (r?.match("incorrect")) return $("#result").value = "プロダクトミスってるぞあほ";
   const res = JSON.parse(r)[product];
-  let result;
-  for (let country in res) {
-    for (let channelId in res[country]) {
-      const channel = res[country][channelId];
-      if (blackCount.includes(channel.count)) continue;
-      if (!result) result = {
-        country,
-        channelId,
-        ...channel
-      };
-      if (result.cost > channel.cost) result = {
-        country,
-        channelId,
-        ...channel
-      };
-    }
-  }
-  $("#result").value = `${product}で一番安いのは、${result.country}のチャネル ${result.channelId} です。
-値段は${result.cost}ロシアルーブルで在庫数は${result.count}個です。`;
+  let result = list(res, 3);
+  $("#result").value = 
+    result.map((x, i) => `${product}で${i}番目に安いのは、${x.country}のチャネル ${x.channelId} です。
+値段は${x.cost}ロシアルーブルで在庫数は${x.count}個です。`).join("\n\n");
   let length = Math.max(...$("#result").value.split("\n").map(x => x.length));
   $("#result").setAttribute("cols", length + 17);
+  $("#result").setAttribute("rows", $("#result").value.split("\n").length+1);
+}
+
+function list(res, i) {
+  const arr = [];
+  for(let j = 0; j < i; j++) {
+    let result;
+    for (let country in res) {
+      for (let channelId in res[country]) {
+        const channel = res[country][channelId];
+        if (blackCount.includes(channel.count)) continue;
+        if (!result) result = {
+          country,
+          channelId,
+          ...channel
+        };
+        if (result.cost > channel.cost) result = {
+          country,
+          channelId,
+          ...channel
+        };
+      }
+    }
+    arr.push(result);
+  }
+  return arr;
 }
