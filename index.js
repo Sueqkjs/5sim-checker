218586b22eeec4437b2c5e6a28172ed319da14bf
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-10-31 21:18:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -27,5 +27,6 @@ $("#check").onclick = async() => {
   }
   $("#result").value = `${product}で一番安いのは、${result.country}のチャネル ${result.channelId} です。
 値段は${result.cost}ロシアルーブルで在庫数は${result.count}個です。`;
-  $("#result").setAttribute("cols", $("#result").value.length+"");
+  let length = Math.max($("#result").value.split("\n").map(x => x.length));
+  $("#result").setAttribute("cols", length+"");
 }
