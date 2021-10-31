24c77dc01b61cb392292c34209e18b1f1306d1da
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-10-31 19:28:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const blackCount = [ 0, 1000, 2000 ];
 
 $("#check").onclick = async() => {
   const product = $("#product").value;
-  const r = await fetch("https://5sim.net/v1/guest/prices?product=" + product, { mode: "no-cors" }).text();
+  const r = await fetch("https://5sim.net/v1/guest/prices?product=" + product, { mode: "no-cors" });
   if (!r.ok) return $("#result").value = "プロダクトミスってるぞあほ";
-  const res = JSON.parse(r)[product];
+  const res = (await r.json())[product];
   let result;
   for (let country in res) {
     for (let channelId in country) {
